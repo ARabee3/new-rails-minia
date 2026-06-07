9f7c80d567dec48eec9ea261a050d8c7e1416c97
ARabee3 <arabeea7104@gmail.com>
2026-06-07 13:51:34 +0300

    Scaffold User and Post resources with one-to-many relationship

---
+++ app/models/user.rb
@@ -0,0 +1,6 @@
+class User < ApplicationRecord
+  has_many :posts, dependent: :destroy
+  has_many :created_posts, class_name: 'Post', foreign_key: 'creator_id', dependent: :destroy
+  has_many :post_editors, dependent: :destroy
+  has_many :edited_posts, through: :post_editors, source: :post
+end
